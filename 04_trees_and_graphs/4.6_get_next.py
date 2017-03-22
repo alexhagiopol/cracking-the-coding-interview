c4ebf835fe4c764dc0757877b4dff3e9649296fa
Alex Hagiopol <alexhagiopol@gmail.com>
2017-03-21 19:46:52 -0700

    fix bug in 4.6

--- 04_trees_and_graphs/4.6_get_next.py
+++ 04_trees_and_graphs/4.6_get_next.py
@@ -11,9 +11,16 @@ class Node:
         return False
 
 
+def get_leftmost(node):
+    candidate = node
+    while candidate.left is not None:
+        candidate = candidate.left
+    return candidate
+
+
 def get_next(node):
     if node.right is not None:
-        return node.right
+        return get_leftmost(node.right)
     if node.parent is None:
         return None
     candidate = node
@@ -26,26 +33,32 @@ def get_next(node):
     return candidate.parent
 
 if __name__ == "__main__":
+    node1 = Node(1)
     node2 = Node(2)
+    node3 = Node(3)
     node4 = Node(4)
+    node5 = Node(5)
     node6 = Node(6)
     node8 = Node(8)
     node10 = Node(10)
-    node20 = Node(20)
 
+    node1.parent = node2
+    node3.parent = node2
     node2.parent = node4
+    node5.parent = node6
     node6.parent = node4
     node4.parent = node8
-    node20.parent = node10
     node10.parent = node8
 
     node8.left = node4
     node8.right = node10
     node4.left = node2
     node4.right = node6
-    node10.right = node20
+    node2.left = node1
+    node2.right = node3
+    node6.left = node5
 
     print "Successor to Node 6 is Node " + str(get_next(node6).val)
     print "Successor to Node 4 is Node " + str(get_next(node4).val)
     print "Successor to Node 2 is Node " + str(get_next(node2).val)
-    print "Successor to Node 20 is " + str(get_next(node20))
+    print "Successor to Node 13 is " + str(get_next(node10))
