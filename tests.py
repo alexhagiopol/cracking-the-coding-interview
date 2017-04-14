b35ef69d2b7b59042a49be9fbfe93cefc09130db
Alex Hagiopol <alexhagiopol@gmail.com>
2017-04-14 09:42:28 -0700

    delete old named files

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-import importlib
+from chapter_02_linked_lists import problem_2_4_partition as p_2_4
+
 
 class Tests(unittest.TestCase):
 
@@ -7,18 +8,18 @@ class Tests(unittest.TestCase):
         pass
 
     def test_problem_2_4(self):
-        problem_2_4 = importlib.import_module("02_linked_lists/2.4_partition.py")
-        n1 = problem_2_4.Node(1, None)
-        n2 = problem_2_4.Node(2, n1)
-        n10 = problem_2_4.Node(10, n2)
-        n5_1 = problem_2_4.Node(5, n10)
-        n8 = problem_2_4.Node(8, n5_1)
-        n5_0 = problem_2_4.Node(5, n8)
-        n3 = problem_2_4.Node(3, n5_0)
-        new_head = problem_2_4.partition(n3)
+        n1 = p_2_4.Node(1, None)
+        n2 = p_2_4.Node(2, n1)
+        n10 = p_2_4.Node(10, n2)
+        n5_1 = p_2_4.Node(5, n10)
+        n8 = p_2_4.Node(8, n5_1)
+        n5_0 = p_2_4.Node(5, n8)
+        n3 = p_2_4.Node(3, n5_0)
+        new_head = p_2_4.partition(n3, 5)
         string_representation = ''
         while new_head is not None:
             string_representation += str(new_head.value)
+            new_head = new_head.next_node
         self.assertEqual(string_representation, '12358510')
 
     def tearDown(self):
