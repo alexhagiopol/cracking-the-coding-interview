770016e5adf58c17d846aebd4240ee03dc95bbe0
Alex Hagiopol <alexhagiopol@gmail.com>
2017-05-23 08:45:51 -0700

    clean up

--- chapter_02_linked_lists/problem_2_4_partition.py
+++ chapter_02_linked_lists/problem_2_4_partition.py
@@ -1,6 +1,3 @@
-from . import LinkedList as ll
-
-
 def partition(node, k):
     tail = node
     head = node
@@ -15,22 +12,3 @@ def partition(node, k):
             head = node
         node = nextNode
     return head
-
-
-if __name__ == "__main__":
-    """
-    Create linked list:
-    3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
-    """
-    n1 = ll.Node(1, None)
-    n2 = ll.Node(2, n1)
-    n10 = ll.Node(10, n2)
-    n5_1 = ll.Node(5, n10)
-    n8 = ll.Node(8, n5_1)
-    n5_0 = ll.Node(5, n8)
-    n3 = ll.Node(3, n5_0)
-    # my_new_head = partition(n3)
-    print("input linked list:")
-    ll.print_list(n3)
-    print("output linked list:")
-    ll.print_list(partition(n3, 5))
