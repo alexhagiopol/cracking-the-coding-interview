345b7a8ea1dcf00751dcbb89303324d0b2c28c36
Alex Hagiopol <alexhagiopol@gmail.com>
2017-05-27 17:27:43 -0700

    style change

--- chapter_01_arrays_strings/problem_1_5_one_away.py
+++ chapter_01_arrays_strings/problem_1_5_one_away.py
@@ -2,21 +2,21 @@ def one_away(s1, s2):
     if abs(len(s1) - len(s2)) > 1:
         return False  # different by more than 1 edit
     i1 = i2 = 0
-    differenceFound = False
+    difference_found = False
     while i1 < len(s1) and i2 < len(s2):
         if s1[i1] == s2[i2]:
             i1 += 1
             i2 += 1
         else:
-            if differenceFound is True:  # there has already been one edit found. different by more than 1 edit
+            if difference_found is True:  # there has already been one edit found. different by more than 1 edit
                 return False
             else:
-                differenceFound = True
+                difference_found = True
                 if len(s1) > len(s2):  # potential insertion
                     i1 += 1
-                elif len(s1) < len(s2): # potential insertion
+                elif len(s1) < len(s2):  # potential insertion
                     i2 += 1
-                else: # potential replacement
+                else:  # potential replacement
                     i1 += 1
                     i2 += 1
     return True  # if we haven't returned by now, the strings are 1 or 0 edits different
