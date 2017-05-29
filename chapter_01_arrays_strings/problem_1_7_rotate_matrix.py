74511b989eca353272efa2dabe8afaf35cc018bf
Alex Hagiopol <alexhagiopol@gmail.com>
2017-05-29 14:44:38 -0700

    use iterative method

--- chapter_01_arrays_strings/problem_1_7_rotate_matrix.py
+++ chapter_01_arrays_strings/problem_1_7_rotate_matrix.py
@@ -1,21 +1,21 @@
+import math
 def rotate_matrix(matrix):
     # assume clockwise rotation
     N, M = matrix.shape
     assert(N == M)  # must be NxN matrix
-    for i in range(N - 1):
-        # from top row to right column
-        right_col_temp = matrix[i, -1]  # save first number from right column
-        matrix[i, -1] = matrix[0, i]
+    for l in range(math.floor(N / 2)):
+        for i in range(l, N - 1 - l):
+            # from top row to right column
+            right_col_temp = matrix[i, - 1 - l]  # save first number from right column
+            matrix[i, - 1 - l] = matrix[l, i]
 
-        # from left column to top row
-        matrix[0, i] = matrix[N - 1 - i, 0]
+            # from left column to top row
+            matrix[l, i] = matrix[N - 1 - i, l]
 
-        # from bottom row to left column
-        matrix[N - 1 - i, 0] = matrix[-1, N - 1 - i]
+            # from bottom row to left column
+            matrix[N - 1 - i, l] = matrix[-1 - l, N - 1 - i]
 
-        # from right column to bottom row
-        matrix[-1, N - 1 - i] = right_col_temp
-
-    if N > 3:
-        matrix[1:-1, 1:-1] = rotate_matrix(matrix[1:-1, 1:-1])
+            # from right column to bottom row
+            matrix[-1 - l, N - 1 - i] = right_col_temp
+            print(matrix)
     return matrix
