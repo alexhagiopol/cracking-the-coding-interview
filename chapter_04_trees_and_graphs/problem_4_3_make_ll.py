ee98763f4fc708fac38ee1c9933ad8fe65058fec
Alex Hagiopol <alexhagiopol@gmail.com>
2017-05-29 22:27:00 -0700

    cleanup

--- chapter_04_trees_and_graphs/problem_4_3_make_ll.py
+++ chapter_04_trees_and_graphs/problem_4_3_make_ll.py
@@ -1,20 +1,10 @@
-class Elem:  # linked list element
-    def __init__(self, val, next_elem=None):
-        self.val = val
-        self.next_elem = next_elem
-
-
-class Node:  # binary tree node
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from . import tree_basics as tb
 
 
 def node_to_ll(list_ll, node, depth):  # traverse tree using dfs and add nodes to LL
     if node is None:
         return
-    last_elem = Elem(node.val, None)
+    last_elem = tb.LLElem(node.val, None)
     if depth <= len(list_ll) - 1:
         penultimate_elem = list_ll[depth][1]
         penultimate_elem.next_elem = last_elem
@@ -27,7 +17,7 @@ def node_to_ll(list_ll, node, depth):  # traverse tree using dfs and add nodes t
 
 
 def make_ll(root):  # set up recursion. note this is the *tree* root we are given
-    first_elem = Elem(root.val, None)  # first *list* element
+    first_elem = tb.LLElem(root.val, None)  # first *list* element
     list_ll = [[first_elem, first_elem]]  # list of linked lists data structure. keep head and tail for O(n) total runtime
     node_to_ll(list_ll, root.left, 1)
     node_to_ll(list_ll, root.right, 1)
