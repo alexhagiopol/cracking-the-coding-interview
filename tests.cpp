2f57bc817ded3700c0be8513c931f3cecd1dc410
Alex Hagiopol <alexhagiopol@gmail.com>
2017-07-31 12:21:07 -0700

    tests hello world

---
+++ tests.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alex on 7/31/17.
+//
+
+#include "cpp_solutions/third_party/Catch/include/catch.hpp"
+#include <iostream>
+
+int main(){
+    std::cout << "Testing C++ Solutions" << std::endl;
+    return 0;
+}
