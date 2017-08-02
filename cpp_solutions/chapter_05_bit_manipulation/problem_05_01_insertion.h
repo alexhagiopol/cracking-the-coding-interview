86ae8cc12e0b544ee35dd3533f52a58a5a0bfff6
Alex Hagiopol <alexhagiopol@gmail.com>
2017-08-02 12:24:50 -0700

    implement 5.1 c++

---
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 8/2/17.
+//
+
+#pragma once
+#include "problem_05_01_insertion.cpp"
+
+int insertion(int N, int M, int i, int j);
--- tests.cpp
+++ tests.cpp
@@ -3,12 +3,16 @@
 //
 
 #include "cpp_solutions/chapter_01_arrays_and_strings/problem_01_01_isUnique.h"
-//#include <iostream>
+#include "cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.h"
 #define CATCH_CONFIG_MAIN
 #include "cpp_solutions/third_party/Catch/include/catch.hpp"
 
 TEST_CASE("Chapter 01 - Problem 01 - isUnique()", "test"){
     REQUIRE(isUnique("alex"));
     REQUIRE(!isUnique("aalex"));
+}
+
+TEST_CASE("Chapter 05 - Problem 01 - insertion()", "test"){
+    REQUIRE(insertion(0b10000000000, 0b10011, 2, 6) == 0b10001001100);
 }
 
