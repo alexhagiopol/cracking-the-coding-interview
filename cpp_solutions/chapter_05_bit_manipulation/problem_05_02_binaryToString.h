5d6cfbec9a8e184c8163e41b003284b3f98076bf
Alex Hagiopol <alexhagiopol@gmail.com>
2017-08-02 20:12:33 -0700

    implement 5.2 c++

---
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_02_binaryToString.h
@@ -0,0 +1,7 @@
+//
+// Created by alex on 8/2/17.
+//
+#pragma once
+#include "problem_05_02_binaryToString.cpp"
+
+int binaryToString(double fraction, std::string& binaryString);
--- tests.cpp
+++ tests.cpp
@@ -4,6 +4,7 @@
 
 #include "cpp_solutions/chapter_01_arrays_and_strings/problem_01_01_isUnique.h"
 #include "cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.h"
+#include "cpp_solutions/chapter_05_bit_manipulation/problem_05_02_binaryToString.h"
 #define CATCH_CONFIG_MAIN
 #include "cpp_solutions/third_party/Catch/include/catch.hpp"
 
@@ -15,4 +16,20 @@ TEST_CASE("Chapter 01 - Problem 01 - isUnique()", "test"){
 TEST_CASE("Chapter 05 - Problem 01 - insertion()", "test"){
     REQUIRE(insertion(0b10000000000, 0b10011, 2, 6) == 0b10001001100);
 }
+
+TEST_CASE("Chapter 05 - Problem 02 - binaryToString()", "test") {
+    std::string binary_string_1 = "";
+    std::string binary_string_2 = "";
+    std::string binary_string_3 = "";
+    std::string binary_string_4 = "";
+    REQUIRE(binaryToString(0.5, binary_string_1) == 0);
+    REQUIRE(binaryToString(0.25, binary_string_2) == 0);
+    REQUIRE(binaryToString(1.5, binary_string_3) == -1);
+    REQUIRE(binaryToString(0.722, binary_string_4) == 0);
+    REQUIRE(binary_string_1 == "0.1");
+    REQUIRE(binary_string_2 == "0.01");
+    REQUIRE(binary_string_3 == "");
+    REQUIRE(binary_string_4 == "0.10111000110101001111110111110011"); // needs double precision to work
+}
+
 
