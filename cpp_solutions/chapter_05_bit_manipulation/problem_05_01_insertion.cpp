b58c0d59662675a27b6d1ed82251b9ce50d5ee8d
Alex Hagiopol <alexhagiopol@gmail.com>
2017-08-03 13:38:22 -0700

    silence warnings

--- cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.cpp
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.cpp
@@ -8,7 +8,7 @@ int insertion(int N, int M, int i, int j){
     example output:    10001001100
     */
     int right_mask = (1 << i) - 1; // produces 0...011
-    int left_mask = -1 << j + 1;  // produces 1...1000000
+    int left_mask = -1 << (j + 1);  // produces 1...1000000
     int M_shifted = M << i;
     int full_mask = right_mask | left_mask;  // produces 1111...000000...1111
     int N_cleared = N & full_mask;  // replace part of N with zeros according to mask
