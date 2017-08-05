d74d537ed90189b58b3f9d92a7d32b872995f0eb
Alex Hagiopol <alexhagiopol@gmail.com>
2017-08-05 15:25:45 -0700

    skeleton for chapter 5 c++

---
+++ cpp_solutions/chapter_01_arrays_and_strings/chapter_01_includes.h
@@ -0,0 +1,11 @@
+//
+// Created by alex on 7/31/17.
+//
+
+#pragma once
+#include "problem_01_01_isUnique.cpp"
+
+namespace chapter_01{
+    bool isUnique(std::string& input);
+}
+
---
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_06_conversion.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Alex Hagiopol on 8/5/17.
+//
+namespace chapter_05{
+    int conversion(int A, int B){
+        return 0;
+    }
+}
+
