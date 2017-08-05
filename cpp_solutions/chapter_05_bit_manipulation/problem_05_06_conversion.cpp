11a7b27119ddf7510140fcd8fa35bde1de0506b3
Alex Hagiopol <alexhagiopol@gmail.com>
2017-08-05 15:34:58 -0700

    implement 5.6 c++

--- cpp_solutions/chapter_05_bit_manipulation/problem_05_06_conversion.cpp
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_06_conversion.cpp
@@ -3,7 +3,20 @@
 //
 namespace chapter_05{
     int conversion(int A, int B){
-        return 0;
+        int count = 0;
+        while(A > 0 || B > 0){
+            if(A & 1){
+                if (!(B & 1)){
+                    count ++;
+                }
+            } else {
+                if (B & 1){
+                    count ++;
+                }
+            }
+            A >>= 1;
+            B >>= 1;
+        }
+        return count;
     }
 }
-
