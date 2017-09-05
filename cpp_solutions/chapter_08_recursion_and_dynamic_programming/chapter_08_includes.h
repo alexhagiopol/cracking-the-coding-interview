e10addd2a3e046b5ce83ace396b6fbafea08dfab
Alex Hagiopol <alexhagiopol@gmail.com>
2017-09-05 13:43:40 -0700

    refactor 8.1

--- cpp_solutions/chapter_08_recursion_and_dynamic_programming/chapter_08_includes.h
+++ cpp_solutions/chapter_08_recursion_and_dynamic_programming/chapter_08_includes.h
@@ -3,7 +3,7 @@
 //
 
 #pragma once
-#include "problem_08_01_tripleStep.cpp"
+#include "problem_08_01_tripleStep.h"
 #include "problem_08_02_robotGrid.cpp"
 #include "problem_08_03_magicIndex.cpp"
 #include "problem_08_04_powerSet.cpp"
@@ -11,7 +11,6 @@
 #include "problem_08_10_paintFill.cpp"
 
 namespace chapter_08{
-    int tripleStep(int n);  // problem 08-01
     bool robotGrid(const Eigen::MatrixXd& maze, std::vector<Eigen::Vector2d>& path);  // problem 08-02
     int magicIndex(const std::vector<int>& array);  // problem 08-03
     void createPowerSet(std::vector<int> set, std::vector<std::vector<int>>& powerSet);  // problem 08-04
