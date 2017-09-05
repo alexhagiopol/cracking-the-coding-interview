58e3705740299e445f912027edb8efaae4a9cb5c
Alex Hagiopol <alexhagiopol@gmail.com>
2017-09-05 13:28:54 -0700

    source restructure 5.1 and 5.2

--- cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.cpp
+++ cpp_solutions/chapter_05_bit_manipulation/problem_05_01_insertion.cpp
@@ -1,6 +1,7 @@
 //
 // Created by alex on 8/2/17.
 //
+#include "problem_05_01_insertion.h"
 
 namespace chapter_05{
     int insertion(int N, int M, int i, int j){
@@ -8,7 +9,7 @@ namespace chapter_05{
         example input: N = 10000000000, M = 10011, i = 2, j = 6
         example output:    10001001100
         */
-        int right_mask = (1 << i) - 1; // produces 0...011
+        int right_mask = (1 << i) - 1;  // produces 0...011
         int left_mask = -1 << (j + 1);  // produces 1...1000000
         int M_shifted = M << i;
         int full_mask = right_mask | left_mask;  // produces 1111...000000...1111
