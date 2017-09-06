3d4d913f44fe9d86b4137990bba768fca0714fef
Alex Hagiopol <alexhagiopol@gmail.com>
2017-09-05 20:28:12 -0700

    add essay 12.2 c++

--- cpp_solutions/chapter_12_cpp/problem_12_02_reverse.cpp
+++ cpp_solutions/chapter_12_cpp/problem_12_02_reverse.cpp
@@ -2,6 +2,24 @@
 // Created by Alex Hagiopol on 9/5/17.
 //
 
+/*
+Chapter 12 - Problem 02 - Reverse - CTCI 6th Edition page 163
+
+Problem Statement:
+Given a string represented as a null terminated char array, reverse it.
+
+Example:
+"Alex" -> "xelA"
+
+Solution:
+Make a copy of the pointer to the front of the string and advance it until it reaches the end of the string.
+Once it reaches the end of the string, advance the front pointer forward and the end pointer backward while
+swapping the pointed-to values along the way using only a single char as a buffer.
+
+Time complexity: O(N)
+Space complexity: O(1)
+*/
+
 #include "problem_12_02_reverse.h"
 #include <iostream>
 #include <string>
