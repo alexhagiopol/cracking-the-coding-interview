941d7969cc3a087a9116152f698f9c3dedd91459
Alex Hagiopol <alexhagiopol@gmail.com>
2017-10-14 15:01:39 -0700

    start 02_03 c++

---
+++ cpp_solutions/chapter_02_linked_lists/problem_02_03_deleteMiddleNode.h
@@ -0,0 +1,33 @@
+//
+// Created by Alex Hagiopol on 10/14/17.
+//
+
+/*
+Chapter 02 - Problem 03 - Delete Middle Node - CTCI 6th Edition page 211
+
+Problem Statement:
+Implement an algorithm to delete a node in the middle (i.e., any node but the first and last
+node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+Example:
+Input: the node c from the linked list a -> b -> c -> d -> e -> f
+Result: nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+
+Solution:
+The solution is best illustrated by example: given node "c", copy the contents of node "d" into node
+"c". Link node "c" to node "e". Delete node "d".
+
+Time complexity: O(1).
+Space complexity: O(1).
+*/
+
+#pragma once
+#include "LinkedListNode.h"
+
+namespace chapter_02{
+    // template function implemented in header file
+    template <typename T>
+    void deleteMiddleNode(LinkedListNode<T>* middle){
+
+    }
+}
