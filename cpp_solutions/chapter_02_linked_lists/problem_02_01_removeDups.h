7880fabf0e2c88d5540059ba963d59247a5c9f18
Alex Hagiopol <alexhagiopol@gmail.com>
2017-10-14 14:51:42 -0700

    implement template functions in header files

--- cpp_solutions/chapter_02_linked_lists/problem_02_01_removeDups.h
+++ cpp_solutions/chapter_02_linked_lists/problem_02_01_removeDups.h
@@ -3,10 +3,28 @@
 //
 #pragma once
 #include "LinkedListNode.h"
+#include <unordered_map>
 
 namespace chapter_02{
+    // template function implemented in header file
     template <typename T>
-    void removeDups(LinkedListNode<T>* head);
+    void removeDups(LinkedListNode<T>* head){
+        LinkedListNode<T>* slowRunner = head;
+        if (slowRunner == nullptr || slowRunner->getNext() == nullptr) return;  // a list of length 0 or 1 cannot have dups
+        LinkedListNode<T>* fastRunner = slowRunner->getNext();
+        std::unordered_map<T, bool> hashTable;
+        hashTable[slowRunner->getValue()] = true;
+        while (fastRunner != nullptr){
+            if (hashTable.count(fastRunner->getValue())){
+                slowRunner->setNext(fastRunner->getNext());
+                fastRunner->setNext(nullptr);
+                delete fastRunner;
+                fastRunner = slowRunner->getNext();
+            } else {
+                hashTable[fastRunner->getValue()] = true;
+                slowRunner = fastRunner;
+                fastRunner = fastRunner->getNext();
+            }
+        }
+    }
 }
-
-#include "problem_02_01_removeDups.cpp"  // include implementation of template function
--- cpp_solutions/chapter_02_linked_lists/problem_02_02_returnKthToLast.h
+++ cpp_solutions/chapter_02_linked_lists/problem_02_02_returnKthToLast.h
@@ -5,8 +5,21 @@
 #include "LinkedListNode.h"
 
 namespace chapter_02{
+    // template function implemented in header file
     template <typename T>
-    LinkedListNode<T>* returnKthToLast(LinkedListNode<T>* head, int k);
+    LinkedListNode<T>* returnKthToLast(LinkedListNode<T>* head, int k){
+        if (head == nullptr) return nullptr;
+        LinkedListNode<T>* slowRunner = head;
+        LinkedListNode<T>* fastRunner = head;
+        while(k > 0){
+            if (fastRunner == nullptr) return nullptr;
+            fastRunner = fastRunner->getNext();
+            k--;
+        }
+        while (fastRunner != nullptr){
+            slowRunner = slowRunner->getNext();
+            fastRunner = fastRunner->getNext();
+        }
+        return slowRunner;
+    }
 }
-
-#include "problem_02_02_returnKthToLast.cpp"  // include implementation of template function
