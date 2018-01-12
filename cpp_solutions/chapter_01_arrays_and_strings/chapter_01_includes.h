76f4d5009bc83c6baf7da2d6c2a429b2292eb9ea
Alex Hagiopol <alexhagiopol@gmail.com>
2018-01-12 11:57:31 -0800

    implement 1.4 c++

--- cpp_solutions/chapter_01_arrays_and_strings/chapter_01_includes.h
+++ cpp_solutions/chapter_01_arrays_and_strings/chapter_01_includes.h
@@ -6,4 +6,5 @@
 #include "problem_01_01_isUnique.h"
 #include "problem_01_02_isPermutation.h"
 #include "problem_01_03_URLify.h"
+#include "problem_01_04_palindromePermutation.h"
 #include "problem_01_07_rotate.h"
