3488d686657dd47e1dd93710a8a74721af0d17d9
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-02-17 18:13:43 -0800

    (1) fix bug where probability of value occurrence did not empirically match expectations (2) refactor random binary tree to only need one class

--- python_solutions/chapter_04_trees_and_graphs/problem_04_11_random_BST.py
+++ python_solutions/chapter_04_trees_and_graphs/problem_04_11_random_BST.py
@@ -3,7 +3,7 @@ import random
 
 class RandBinNode:
 
-    def __init__(self, value, left=None, right=None, children=0):
+    def __init__(self, value=None, left=None, right=None, children=0):
         self.value = value
         self.left = left
         self.right = right
@@ -21,48 +21,35 @@ class RandBinNode:
         else:
             return 1 + self.right.children
 
-
-class RandBST:
-    def __init__(self, root=None):
-        self.root = root
-
     def insert(self, value):
-        if self.root is None:
-            self.root = RandBinNode(value)
+        if value is None:  # like good silicon valley people, we disallow adding no value
             return
-        runner = self.root
-        while 1:
-            if value > runner.value:
-                runner.children += 1
-                if runner.right is not None:
-                    runner = runner.right
-                else:
-                    runner.right = RandBinNode(value)
-                    return
-            elif value < runner.value:
-                runner.children += 1
-                if runner.left is not None:
-                    runner = runner.left
-                else:
-                    runner.left = RandBinNode(value)
-                    return
+        if self.value is None:  # create children only if the current node has no value
+            self.value = value
+            return
+        self.children += 1
+        if value > self.value:
+            if self.right is not None:
+                self.right.insert(value)
             else:
-                return  # don't allow non-unique values in tree
+                self.right = RandBinNode(value)
+        else:
+            if self.left is not None:
+                self.left.insert(value)
+            else:
+                self.left = RandBinNode(value)
+        return
 
     def get_random(self):
-        if self.root is None:
-            return None
-        runner = self.root
-        while 1:
-            assert(runner.children == runner.left_children() + runner.right_children())  #
-            sum_of_nodes = 1 + runner.children
-            rand_int = random.randint(1, sum_of_nodes)
-            if rand_int == 1:
-                return runner.value
-            elif 1 < rand_int <= 1 + runner.left_children():
-                runner = runner.left
-            elif 1 + runner.left_children() < rand_int:
-                runner = runner.right
+        assert(self.children == self.left_children() + self.right_children())
+        num_nodes = 1 + self.children
+        rand_int = random.randint(1, num_nodes)
+        if rand_int == 1:
+            return self.value
+        elif 1 < rand_int <= 1 + self.left_children():
+            return self.left.get_random()
+        elif 1 + self.left_children() < rand_int:
+            return self.right.get_random()
 
     def find(self):
         pass
