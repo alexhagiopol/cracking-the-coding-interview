8d39f21aeceba4636452c48abf0e4dd2376abacf
pavel1shatalov <43522734+pavel1shatalov@users.noreply.github.com>
2019-04-13 23:24:27 +0300

    Update problem_02_01_remove_dups.py
    1) An element after next can also be a duplicate.
    Example: 
    5->5->5->4
    right after you'll have:
    5->4
    skipping two duplicates instead of one.
    
    P.S. Thank you for all your hard work!

--- python_solutions/chapter_02_linked_lists/problem_02_01_remove_dups.py
+++ python_solutions/chapter_02_linked_lists/problem_02_01_remove_dups.py
@@ -1,22 +1,22 @@
 def remove_dups(head):  # O(n) speed with O(n) space for hash table
     hash_table = {head.value: True}
     while head is not None:
-        if head.next_node is not None:
-            if hash_table.get(head.next_node.value, None) is not None:
-                head.next_node = head.next_node.next_node
-            else:
-                hash_table[head.next_node.value] = True
-        head = head.next_node
+        if head.next_node is None:
+            break
+        if hash_table.get(head.next_node.value) is not None:
+            head.next_node = head.next_node.next_node
+        else:
+            hash_table[head.next_node.value] = True
+            head = head.next_node
 
 
 def remove_dups_alternative(head):  # O(n^2) speed with O(1) space due to no hash table
     while head is not None:
         runner = head
         while runner is not None:
-            if runner.next_node is not None and runner.next_node.value == head.value:
-                if runner.next_node is None:
-                    break
-                else:
-                    runner.next_node = runner.next_node.next_node
+            if runner.next_node is None:
+                break
+            if runner.next_node.value == head.value:
+                runner.next_node = runner.next_node.next_node
             runner = runner.next_node
         head = head.next_node
