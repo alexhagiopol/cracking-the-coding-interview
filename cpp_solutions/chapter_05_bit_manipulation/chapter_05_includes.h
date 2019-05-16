a3b7b86c1d84c95397789e862581d0974186c704
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-05-16 06:30:42 -0800

    interface for 5.3 C++

--- cpp_solutions/chapter_05_bit_manipulation/chapter_05_includes.h
+++ cpp_solutions/chapter_05_bit_manipulation/chapter_05_includes.h
@@ -6,5 +6,6 @@
 
 #include "problem_05_01_insertion.h"
 #include "problem_05_02_binaryToString.h"
+#include "problem_05_03_flipBitToWin.h"
 #include "problem_05_06_conversion.h"
 #include "problem_05_07_pairwiseSwap.h"
