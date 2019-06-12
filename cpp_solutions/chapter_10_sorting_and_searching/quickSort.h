0dde197452b7af2155bf5b2b109f42a085235a7d
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-06-12 14:37:14 -0700

    implement quickSort in Python

--- cpp_solutions/chapter_10_sorting_and_searching/quickSort.h
+++ cpp_solutions/chapter_10_sorting_and_searching/quickSort.h
@@ -5,26 +5,24 @@ namespace chapter_10 {
     template <typename T>
     int partition(std::vector<T>& array, int start, int end) {
         T partitionValue = array[end];
-        int swapIndex = start;
+        int partitionIndex = start;
         // perform parition operation:
         // swap element to lowest index with unknown partition assignment
         for (int i = start; i <= end; i++) {
             if (array[i] <= partitionValue) {
-                std::swap(array[i], array[swapIndex]);
-                swapIndex ++;
+                std::swap(array[i], array[partitionIndex]);
+                partitionIndex ++;
             }
         }
-        return swapIndex - 1;  // - 1 to undo the increment operation after the final swap of the partitionValue
+        return partitionIndex - 1;  // - 1 to undo the increment operation after the final swap of the partitionValue
     }
 
     template <typename T>
     void quickSortHelper(std::vector<T>& array, int start, int end) {
         if (start < end) {
             int partitionIndex = partition(array, start, end);
-            if (partitionIndex >= start && partitionIndex <= end) {
-                quickSortHelper(array, start, partitionIndex - 1);
-                quickSortHelper(array, partitionIndex + 1, end);
-            }
+            quickSortHelper(array, start, partitionIndex - 1);
+            quickSortHelper(array, partitionIndex + 1, end);
         }
     }
 
