59cb4e7cb590d55d45dc31e048b0c2ce99f94ea6
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-07-01 22:35:46 -0700

    use smaller dataset

--- cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
+++ cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
@@ -7,14 +7,17 @@ int main() {
     std::string filename = "random_number_dataset.csv";
     std::cout << "Random number file generator outputting to " << filename << std::endl;
     // initialize random integer generator
+    // problem 10.7 specifies a max value of 2^32 = 0xFFFFFFFF ~= 4,294,967,296
+    // for reasonable test dataset size, we use a smaller range of [0,4300]
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint32_t> dis(0, 0xFFFFFFFF);
+    std::uniform_int_distribution<uint32_t> dis(0, 11000);
     // initialize file stream
     std::ofstream file;
     file.open(filename);
     // write to file line by line
-    for (int i = 0; i < 10000; i++) {  // problem 10.7 specifies 4 billion numbers, for testing speed, we only write 10000
+    // problem 10.7 specifies 4 billion numbers, for testing speed, we only write 4000
+    for (int i = 0; i < 4000; i++) {
         file << dis(gen);
         file << ",\n";
     }
