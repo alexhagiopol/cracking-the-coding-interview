f7018e8b8b06802a2fd733aa58dcf445e6bbcd0e
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-07-01 22:29:26 -0700

    WIP 10.7 make dataset generator

--- cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
+++ cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <string>
 
-int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		std::cout << "Usage: ./generate_random_number_file title.csv" << std::endl;
-	}
-	std::cout << "Hello World." << std::endl;
+int main() {
+    std::string filename = "random_number_dataset.csv";
+    std::cout << "Random number file generator outputting to " << filename << std::endl;
+    // initialize random integer generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint32_t> dis(0, 0xFFFFFFFF);
+    // initialize file stream
+    std::ofstream file;
+    file.open(filename);
+    // write to file line by line
+    for (int i = 0; i < 10000; i++) {  // problem 10.7 specifies 4 billion numbers, for testing speed, we only write 10000
+        file << dis(gen);
+        file << ",\n";
+    }
+    file.close();
 	return 0;
 }
