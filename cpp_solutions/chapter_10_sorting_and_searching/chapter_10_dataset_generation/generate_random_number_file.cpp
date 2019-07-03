562f24e8033807af090d7673c1bfcdf4300c89ed
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-07-02 21:33:52 -0700

    implement 10.7 C++

--- cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
+++ cpp_solutions/chapter_10_sorting_and_searching/chapter_10_dataset_generation/generate_random_number_file.cpp
@@ -6,18 +6,21 @@
 int main() {
     std::string filename = "random_number_dataset.csv";
     std::cout << "Random number file generator outputting to " << filename << std::endl;
-    // initialize random integer generator
     // problem 10.7 specifies a max value of 2^32 = 0xFFFFFFFF ~= 4,294,967,296
     // for reasonable test dataset size, we use a smaller range of [0,4300]
+    const int numberRange = 1000;
+    // problem 10.7 specifies 4 billion numbers, for testing speed, we only write 4000
+    const int numberCount = 4000;
+
+    // initialize random integer generator
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint32_t> dis(0, 11000);
+    std::uniform_int_distribution<uint32_t> dis(0, numberRange);
     // initialize file stream
     std::ofstream file;
     file.open(filename);
     // write to file line by line
-    // problem 10.7 specifies 4 billion numbers, for testing speed, we only write 4000
-    for (int i = 0; i < 4000; i++) {
+    for (int i = 0; i < numberCount; i++) {
         file << dis(gen);
         file << ",\n";
     }
--- cpp_solutions/chapter_10_sorting_and_searching/problem_10_07_missingInt.cpp
+++ cpp_solutions/chapter_10_sorting_and_searching/problem_10_07_missingInt.cpp
@@ -6,14 +6,23 @@
 
 namespace chapter_10 {
     uint32_t missingInt(const std::string& filename) {
-        std::cout << "filename=" << filename << std::endl;
-        /*
+        // problem 10.7 specifies a max value of 2^32 = 0xFFFFFFFF ~= 4,294,967,296
+        // for reasonable test dataset size, we use a smaller range of [0,2000]
+        // create a bitVector with one bit for each possible number
+        const int numberRange = 1000;
+        bool bitVector[numberRange + 1] = {0};
+        // problem 10.7 specifies 4 billion numbers, for testing speed, we only read 4000
+        // read integers from file
         std::ifstream file(filename);
         std::string line = "";
         while (std::getline(file, line)){
-            std::cout << line << std::endl;
+            uint32_t number = static_cast<uint32_t>(std::stoi(line));
+            if (0 <= number && number <= numberRange && !bitVector[number]) bitVector[number] = true;
         }
-         */
-        return 0;
+        // search bitVector for first occurrence of unique number
+        for (uint32_t i = 0; i <= numberRange; i++) {
+            if (!bitVector[i]) return static_cast<uint32_t>(i);
+        }
+        return -1;
     }
 }
