02115d83d250d353d4eb09f111027e208b75065c
Alex Hagiopol <alex.hagiopol@icloud.com>
2019-08-12 14:15:22 -0700

    switch integral image implementation to shared pointers

--- cpp_solutions/misc_exercises/integralImage.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Eigen/Dense>
-
-namespace misc{
-    Eigen::MatrixXd makeIntegralImage(const Eigen::MatrixXd& input);
-    Eigen::MatrixXd boxFilter(const Eigen::MatrixXd& input, int k);  // assume k x k box filter with all values = to 1
-}
-
