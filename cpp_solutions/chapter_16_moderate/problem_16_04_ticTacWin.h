2f94060a357561c3bdd29b62144902d343f5eaf5
Alex Hagiopol <alex.hagiopol@icloud.com>
2020-01-03 17:42:21 -0800

    add essay for 16.4 C++

--- cpp_solutions/chapter_16_moderate/problem_16_04_ticTacWin.h
+++ cpp_solutions/chapter_16_moderate/problem_16_04_ticTacWin.h
@@ -1,3 +1,20 @@
+/* PROBLEM:
+ * Design an algorithm to figure out if someone has won a game of tic-tac-toe.
+ *
+ * SOLUTION:
+ * 1. An algorithm to check if a TTT board is a winning board. Check all rows, all cols, and both diagonals for equality.
+ * 2. An algorithm to store the wiining-ness of all possible TTT boards in a hash map. Generate every possible TTT board
+ * using a recursion tree that assigns one of three possible cell states (blank, X, or O) to one of 9 cells at each level
+ * of the recursion.
+ * 3. A class that populates the hash map in (2) upon construction and only queries the hash map when
+ * asked to determine if a TTT board is a winning board.
+ *
+ * SPACE / TIME:
+ * If we assume that there will be N calls to determine if a TTT board is a winning board, each call will require O(1)
+ * time and O(1) space will be required to store the hash table.
+ *
+ */
+
 #pragma once
 #include <unordered_map>
 
